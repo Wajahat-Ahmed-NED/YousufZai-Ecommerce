24e9784660cd5f911870eb8514cad842c2aa93c8
Wajahat-Ahmed-NED <wajahatahmedwajahat@gmail.com>
2023-10-22 01:36:46 +0500

    test

--- frontend/src/actions/userAction.js
+++ frontend/src/actions/userAction.js
@@ -107,7 +107,7 @@ export const updateProfile = (userData) => async (dispatch) => {
     const config = { headers: { "Content-Type": "multipart/form-data" } };
 
     const { data } = await axios.put(`https://yousuf-zai-ecommerce-api.vercel.app//api/v1/me/update`, userData, config);
-
+ 
     dispatch({ type: UPDATE_PROFILE_SUCCESS, payload: data.success });
   } catch (error) {
     dispatch({
