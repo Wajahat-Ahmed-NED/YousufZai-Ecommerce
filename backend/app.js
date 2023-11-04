57ab611563fee77da0740e885e8a933213f23dd0
Wajahat-Ahmed-NED <wajahatahmedwajahat@gmail.com>
2023-11-05 01:24:26 +0500

    build added in app.js

--- backend/app.js
+++ backend/app.js
@@ -15,9 +15,9 @@ app.use(
 const errorMiddleware = require("./middleware/error");
 
 // Config
-// if (process.env.NODE_ENV !== "PRODUCTION") {
-//   require("dotenv").config({ path: "backend/config/config.env" });
-// }
+if (process.env.NODE_ENV !== "PRODUCTION") {
+  require("dotenv").config({ path: "backend/config/config.env" });
+}
 
 app.use(express.json());
 app.use(cookieParser());
@@ -35,12 +35,14 @@ app.use("/api/v1", user);
 app.use("/api/v1", order);
 app.use("/api/v1", payment);
 
-// app.use(express.static(path.join(__dirname, "../frontend/build")));
+app.use(express.static(path.join(__dirname, "../frontend/build")));
 
 app.get("/", (req, res) => {
   res.send("Success");
 });
-
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
+});
 // Middleware for Errors
 app.use(errorMiddleware);
 
