77381bbe5b3c80b260db9f28728172390e587533
Wajahat-Ahmed-NED <wajahatahmedwajahat@gmail.com>
2023-11-05 01:32:09 +0500

    / api removed

--- backend/app.js
+++ backend/app.js
@@ -37,9 +37,9 @@ app.use("/api/v1", payment);
 
 app.use(express.static(path.join(__dirname, "../frontend/build")));
 
-app.get("/", (req, res) => {
-  res.send("Success");
-});
+// app.get("/", (req, res) => {
+//   res.send("Success");
+// });
 app.get("*", (req, res) => {
   res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
 });
