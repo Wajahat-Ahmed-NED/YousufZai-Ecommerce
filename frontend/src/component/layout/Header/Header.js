0e74f645780a0549ba54d67ef9d6b0f217a13f58
Wajahat-Ahmed-NED <wajahatahmedwajahat@gmail.com>
2023-12-16 01:41:26 +0500

    icons fetched again in overlay-navbar

--- frontend/src/component/layout/Header/Header.js
+++ frontend/src/component/layout/Header/Header.js
@@ -1,6 +1,10 @@
 import React from "react";
 import { ReactNavbar } from "overlay-navbar";
 import logo from "../../../images/download.png";
+// import DashboardIcon from "@material-ui/icons/Dashboard";
+import { MdAccountCircle } from "react-icons/md";
+import { MdSearch } from "react-icons/md";
+import { MdAddShoppingCart } from "react-icons/md";
 
 const options = {
   burgerColorHover: "#eb4034",
@@ -13,23 +17,29 @@ const options = {
   logoHoverColor: "#eb4034",
   link1Text: "Home",
   link2Text: "Courses",
-  // link3Text: "Trainers",
   link3Text: "Trainers",
   link4Text: "About Us",
   link1Url: "/",
   link2Url: "/products",
-  // link3Url: "/trainer",
   link3Url: "/contact",
   link4Url: "/about",
   link1Size: "1.3vmax",
   link1Color: "rgba(35, 35, 35,0.8)",
   nav1justifyContent: "flex-end",
   nav2justifyContent: "flex-end",
-  // nav3justifyContent: "flex-end",
   nav3justifyContent: "flex-start",
   nav4justifyContent: "flex-start",
   link1ColorHover: "#eb4034",
   link1Margin: "1vmax",
+  profileIcon: true,
+  profileIconColor: "rgba(35, 35, 35,0.8)",
+  ProfileIconElement: MdAccountCircle,
+  searchIcon: true,
+  searchIconColor: "rgba(35, 35, 35,0.8)",
+  SearchIconElement: MdSearch,
+  cartIcon: true,
+  cartIconColor: "rgba(35, 35, 35,0.8)",
+  CartIconElement: MdAddShoppingCart,
   profileIconUrl: "/login",
   profileIconColor: "rgba(35, 35, 35,0.8)",
   searchIconColor: "rgba(35, 35, 35,0.8)",
@@ -41,16 +51,7 @@ const options = {
 };
 
 const Header = () => {
-  return (
-    <ReactNavbar
-      {...options}
-      // style={{
-      //   backgroundColor: "transparent !important",
-      //   zIndex: 1000,
-      //   marginLeft: "-15px",
-      // }}
-    />
-  );
+  return <ReactNavbar {...options} />;
 };
 
 export default Header;
